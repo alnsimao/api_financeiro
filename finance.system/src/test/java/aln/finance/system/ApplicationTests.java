5f6ddc3277fcd9cb8e0995f8129e3e4adb7fd7d8
alnsimao <alansimao9a1@gmail.com>
2026-01-13 11:42:42 -0300

    Initial Spring Boot project

---
+++ finance.system/src/test/java/aln/finance/system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package aln.finance.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
