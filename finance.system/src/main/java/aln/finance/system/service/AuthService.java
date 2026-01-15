7e3c9fa82eae5f1b3ccdf838601de0d5f1533509
alnsimao <alansimao9a1@gmail.com>
2026-01-15 11:05:55 -0300

    wip(auth): initial login logic implementation

--- finance.system/src/main/java/aln/finance/system/service/AuthService.java
+++ finance.system/src/main/java/aln/finance/system/service/AuthService.java
@@ -1,10 +1,14 @@
 package aln.finance.system.service;
 
+import aln.finance.system.dto.LoginRequest;
 import aln.finance.system.model.User;
 import aln.finance.system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -21,5 +25,18 @@ public class AuthService {
             throw new RuntimeException("User with email " + user.getEmail() + " already exists");
         user.setPassword(encoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    public LoginRequest login(LoginRequest loginRequest) {
+        Optional<User> userLogin = userRepository.findByEmail(loginRequest.getEmail());
+
+        if(userLogin.isEmpty())
+            throw new RuntimeException("User not found");
+
+        User user = userLogin.get();
+
+        String hashedPassword = user.getPassword();
+        boolean matches = encoder.matches(loginRequest.getPassword(), hashedPassword);
+
     }
 }
