2480b31d54bbdee2543da43d8c239a9e88f6d438
alnsimao <alansimao9a1@gmail.com>
2026-01-15 10:23:17 -0300

    feature(Created LoginRequest)

---
+++ finance.system/src/main/java/aln/finance/system/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package aln.finance.system.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginRequest {
+    public String email;
+    public String password;
+}
