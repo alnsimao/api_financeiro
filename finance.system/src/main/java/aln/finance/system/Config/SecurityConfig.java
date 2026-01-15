85af079df7cd6298bd5a39e0c13625ff142869e8
alnsimao <alansimao9a1@gmail.com>
2026-01-15 09:30:33 -0300

    feat(Created package config: created SecurityConfig)

---
+++ finance.system/src/main/java/aln/finance/system/Config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package aln.finance.system.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+}
