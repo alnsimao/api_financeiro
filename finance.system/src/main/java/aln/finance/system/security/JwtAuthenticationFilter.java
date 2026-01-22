ed860b26a28f675eaa589528eb1c90ecf9298201
alnsimao <alansimao9a1@gmail.com>
2026-01-22 11:43:59 -0300

    feat(JwtAuthenticationFilter: Created method for Jwt filter)

--- finance.system/src/main/java/aln/finance/system/security/JwtAuthenticationFilter.java
+++ finance.system/src/main/java/aln/finance/system/security/JwtAuthenticationFilter.java
@@ -1,4 +1,45 @@
 package aln.finance.system.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.net.PasswordAuthentication;
+import java.util.Collection;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    String authHeader = request.getHeader("Authorization");
+    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        filterChain.doFilter(request, response);
+        return;
+    }
+    String token = authHeader.substring(7);
+
+    if(!jwtUtil.validateToken(token)){
+        filterChain.doFilter(request, response);
+        return;
+    }
+    String email = jwtUtil.getEmailFromToken(token);
+
+        UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(
+                        email,
+                        null,
+                        Collections.emptyList());
+    }
 }
