67bb026485a9633f95c291922ac3c90b25f01959
alnsimao <alansimao9a1@gmail.com>
2026-02-09 08:44:22 -0300

    feat(TransactionRepository: Add and refactor methods)

--- finance.system/src/main/java/aln/finance/system/repository/TransactionRepository.java
+++ finance.system/src/main/java/aln/finance/system/repository/TransactionRepository.java
@@ -5,12 +5,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.Date;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     Page<Transaction> findByUser_Id(Long userId, Pageable pageable);
-    Page<Transaction> findByUser_IdAndDateBetween(Long userId, Date date, Pageable pageable);
+    Page<Transaction> findByUser_IdAndDateBetween(Long userId, LocalDate initialDate,LocalDate finalDate, Pageable pageable);
     Page<Transaction> findByUser_IdAndCategory_Id(Long userId, Long categoryId, Pageable pageable);
-    Page<Transaction> findByUser_IdAndAmountGreaterThan(Long userId, Long amount, Pageable pageable);
+    Page<Transaction> findByUser_IdAndAmountGreaterThan(Long userId, BigDecimal amount, Pageable pageable);
+    Page<Transaction> findByUser_IdAndAmountLessThan(Long userId, BigDecimal amount, Pageable pageable);
+    Page<Transaction> findByUser_IdAndAmountBetween(Long userId, BigDecimal initialAmount,BigDecimal finalAmount, Pageable pageable);
 }
