59743fee0a063c4c2fbff378b9177368b2448424
alnsimao <alansimao9a1@gmail.com>
2026-02-09 08:25:11 -0300

    feat(Created TransactionRepository and your methods)

--- finance.system/src/main/java/aln/finance/system/repository/TransactionRepository.java
+++ finance.system/src/main/java/aln/finance/system/repository/TransactionRepository.java
@@ -1,8 +1,16 @@
 package aln.finance.system.repository;
 
 import aln.finance.system.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.sql.Date;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
+    Page<Transaction> findByUser_Id(Long userId, Pageable pageable);
+    Page<Transaction> findByUser_IdAndDateBetween(Long userId, Date date, Pageable pageable);
+    Page<Transaction> findByUser_IdAndCategory_Id(Long userId, Long categoryId, Pageable pageable);
+    Page<Transaction> findByUser_IdAndAmountGreaterThan(Long userId, Long amount, Pageable pageable);
 }
