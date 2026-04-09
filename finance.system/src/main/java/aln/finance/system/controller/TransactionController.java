c1b149fe6d6442674cf1307eda8495ce8cfd2ff9
alnsimao <alansimao9a1@gmail.com>
2026-04-09 16:02:38 -0300

    feat(Created CRUD for transaction)

--- finance.system/src/main/java/aln/finance/system/controller/TransactionController.java
+++ finance.system/src/main/java/aln/finance/system/controller/TransactionController.java
@@ -2,6 +2,7 @@ package aln.finance.system.controller;
 
 import aln.finance.system.dto.GetTransactionResponseDTO;
 import aln.finance.system.dto.TransactionDTO;
+import aln.finance.system.dto.TransactionFilterDTO;
 import aln.finance.system.model.Transaction;
 import aln.finance.system.service.AuthService;
 import aln.finance.system.service.TransactionService;
@@ -10,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -23,13 +27,42 @@ public class TransactionController {
     public Transaction createTransaction(@Valid @RequestBody TransactionDTO transaction) {
         Long userId = authService.getLoggedUserId();
         Long categoryId = transaction.getCategoryId();
-        return transactionService.createTransaction(transaction,userId,categoryId);
+        return transactionService.createTransaction(transaction, userId, categoryId);
     }
 
-     @GetMapping("/list")
+    @GetMapping("/list")
     public Page<GetTransactionResponseDTO> listTransaction(Pageable page) {
         Long userId = authService.getLoggedUserId();
-        return transactionService.getTransaction(userId,page);
+        return transactionService.getTransaction(userId, page);
+    }
+
+    @GetMapping
+    public Page<GetTransactionResponseDTO> getTransactions(
+            @RequestParam(required = false) LocalDate initialDate
+            , @RequestParam(required = false) LocalDate finalDate
+            , @RequestParam(required = false) Long categoryId
+            , @RequestParam(required = false) BigDecimal minAmount
+            , @RequestParam(required = false) BigDecimal maxAmount,
+            Pageable page) {
+        Long userId = authService.getLoggedUserId();
+
+        TransactionFilterDTO filter = new TransactionFilterDTO(initialDate,finalDate, categoryId, minAmount, maxAmount);
+        return transactionService.getTransactionsWithFilters(userId, filter, page);
+    }
+        @PutMapping("/{id}")
+        public Transaction updateTransaction(@PathVariable Long id, @Valid @RequestBody TransactionDTO transaction) {
+            Long userId = authService.getLoggedUserId();
+           return transactionService.editTransaction(
+                   id,
+                   transaction,
+                   userId,
+                   transaction.getCategoryId()
+           );
+        }
+    @DeleteMapping("/{id}")
+    public void deleteTransaction(@PathVariable Long id) {
+        Long userId = authService.getLoggedUserId();
+        transactionService.deleteTransaction(id, userId);
     }
 
 
