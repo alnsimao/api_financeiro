ebd66e1a077bc9a0a500955494c6e552f8c1a090
alnsimao <alansimao9a1@gmail.com>
2026-04-28 10:29:02 -0300

    feat(TransactionRepository: Created JPQL query for SUM by categorys)

--- finance.system/src/main/java/aln/finance/system/repository/TransactionRepository.java
+++ finance.system/src/main/java/aln/finance/system/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package aln.finance.system.repository;
 
+import aln.finance.system.dto.CategorySummaryDTO;
 import aln.finance.system.model.Transaction;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
@@ -25,4 +27,10 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long>,
             "AND t.category.type = 'EXPENSE' " +
             "AND t.date BETWEEN :start AND :end")
     BigDecimal sumExpense(Long userId, LocalDate start, LocalDate end);
+
+    @Query("SELECT new aln.finance.system.dto.CategorySummaryDTO(t.category.name, SUM(t.amount))" +
+    "FROM Transaction t" +
+    " WHERE t.user.id = :userId AND t.date BETWEEN :start AND :end"+
+    "GROUP BY t.category.name")
+    List<CategorySummaryDTO> sumByCategory(Long userId, LocalDate start, LocalDate end);
 }
