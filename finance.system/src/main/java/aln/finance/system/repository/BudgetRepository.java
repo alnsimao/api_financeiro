bfb6371467075ccddd9f2e04cd05ee4dab8a7810
alnsimao <alansimao9a1@gmail.com>
2026-05-04 17:04:24 -0300

    feat(Created: BudgetRepository and BudgetService)

---
+++ finance.system/src/main/java/aln/finance/system/repository/BudgetRepository.java
@@ -0,0 +1,8 @@
+package aln.finance.system.repository;
+
+import aln.finance.system.model.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BudgetRepository extends JpaRepository<Budget, Long> {
+
+}
---
+++ finance.system/src/main/java/aln/finance/system/service/BudgetService.java
@@ -0,0 +1,7 @@
+package aln.finance.system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BudgetService {
+}
