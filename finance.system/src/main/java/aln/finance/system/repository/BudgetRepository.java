bedcf7e9f4bb82f24a01c8e5166a7ced8912a222
alnsimao <alansimao9a1@gmail.com>
2026-05-04 17:10:44 -0300

    feat(BudgetRepository: Add 2 new methods for find by userId,CategoryId and Period)

--- finance.system/src/main/java/aln/finance/system/repository/BudgetRepository.java
+++ finance.system/src/main/java/aln/finance/system/repository/BudgetRepository.java
@@ -3,6 +3,12 @@ package aln.finance.system.repository;
 import aln.finance.system.model.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
+    Optional<Budget> findByUserIdAndCategoryIdAndPeriod(Long userId,Long categoryId, Budget.BudgetPeriod period);
+
+    List<Budget> findByUserId(Long userId);
 
 }
