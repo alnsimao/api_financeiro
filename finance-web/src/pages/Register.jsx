77a2e0bbf2d1e8acd8e451ef5042d6a1fe04bea5
alnsimao <alansimao9a1@gmail.com>
2026-05-22 15:26:27 -0300

    feat(Download TailWind Css)

--- finance-web/src/pages/Register.jsx
+++ finance-web/src/pages/Register.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 function Register() {
   return (
     <div style={{ padding: '20px', maxWidth: '300px', margin: '50px auto' }}>
+        <h1 className="text-4xl font-bold text-blue-600">Testando Tailwind!</h1>
       <h2>Criar Conta</h2>
       {/* Aqui vai o formulário de cadastro */}
       <p>Já tem conta? <Link to="/login">Faça Login</Link></p>
